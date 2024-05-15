d888caf8593bc540e5ad73c7baf7693e838abf1a
Rodrigo Arede <aredemiguel@gmail.com>
2024-05-15 17:16:52 +0100

    idea

--- besu/src/contracts/RentalAgreement.sol
+++ besu/src/contracts/RentalAgreement.sol
@@ -9,20 +9,12 @@ import "./interface/IERC20.sol";
 
 contract RentalAgreement is PaymentSplitter {
 
-    modifier expired() { // TODO a usar para funcoes tipo force terminate
-        require(block.timestamp - paymentExpirationDate() >= PERIOD, "RentalAgreement: period has not passed since last payment");
-        _;
-    }
+    event RentalEnrolled(address indexed tenant, address indexed landlord, address indexed realty);
+    event RentalConcluded(address indexed tenant, address indexed landlord, address indexed realty);
+    event RentalTerminated(address indexed tenant, address indexed landlord, address indexed realty);
+    event SecurityDepositReturned(address indexed tenant, address indexed landlord, address indexed realty, uint value);
+    event RentPayment(address indexed tenant, address indexed landlord, address indexed realty, uint value);
 
-    modifier canPay() { // TODO recheck this, necessario ter paymentCounter == 0? 
-        require(paymentCounter == 0 || (block.timestamp >= terms.startDate + ((paymentCounter-1) * PERIOD)), "RentalAgreement: cannot pay yet");
-        _;
-    }
-
-    modifier rentTimeOver() {
-        require(block.timestamp >= terms.startDate  + (terms.duration * PERIOD), "RentalAgreement: rent time is not over");
-        _;
-    }
 
     modifier active() {
         require(status == RentStatus.ACTIVE, "RentalAgreement: rent agreement is not active");
@@ -49,7 +41,8 @@ contract RentalAgreement is PaymentSplitter {
         _;
     }
 
-    enum RentStatus { PENDING, ACTIVE, COMPLETED, CONCLUDED, TERMINATED, EVICTED }
+    enum RentStatus { PENDING, ACTIVE, COMPLETED, TERMINATED}
+    enum Action { RENEW, TERMINATE }
     uint public constant PERIOD = 30 days;
     
     struct RentalTerms {
@@ -69,12 +62,12 @@ contract RentalAgreement is PaymentSplitter {
     address public tenant;
     RentStatus public status;
     uint public paymentCounter;
-    string public review;
 
     uint public penaltySum;
 
-    uint public renewalRequested;
-    address public renewalRequester;
+    Action public tenantAction;
+    Action public tenantAction;
+    uint public actionParam;
 
     constructor(address _cns, address _tenant, RentalTerms memory _terms) 
         PaymentSplitter(_terms.payees, _terms.shares, _cns) 
@@ -94,9 +87,11 @@ contract RentalAgreement is PaymentSplitter {
         require(status == RentStatus.PENDING, "RentalAgreement: rent agreement is not active");
         IERC20(walletContractAddress()).transferFrom(tenant, address(this), terms.securityDeposit);
         status = RentStatus.ACTIVE;
+        payRent();
     }
 
-    function payRent() public active onlyTenant canPay {
+    function payRent() public active onlyTenant {
+        require(paymentCounter == 0 || (block.timestamp >= terms.startDate + ((paymentCounter-1) * PERIOD)), "RentalAgreement: cannot pay rent yet");
         super.pay(terms.rentValue + penaltySum);
         penaltySum = 0;
         if (++paymentCounter == terms.duration) {
@@ -104,22 +99,15 @@ contract RentalAgreement is PaymentSplitter {
         }
     }
 
-    function conclude(uint _penalty) public onlyLandlord completed rentTimeOver {
-        require(_penalty < terms.securityDeposit, "RentalAgreement: penalty must be less than the security deposit");
-        uint remaining = terms.securityDeposit - _penalty;
-        if (remaining > 0) {
-            IERC20(walletContractAddress()).transfer(tenant, remaining);
-        }
-        if (_penalty > 0) {
-            super.collect();
-        }
-        status = RentStatus.CONCLUDED;
+    function terminate() public onlyParties active {
+        require(block.timestamp >= terms.startDate  + (terms.duration * PERIOD), "RentalAgreement: rent time is not over");
+
+        status = RentStatus.COMPLETED;
     }
 
-    function evict() public onlyLandlord active expired {
-        require(block.timestamp - terms.startDate >= 1 days, "RentalAgreement: cannot dump tenant on first day");
-        status = RentStatus.EVICTED;
-        super.collect(); // security deposit splitted
+    function evict() public onlyLandlord active {
+        require(block.timestamp - paymentDueDate() >= PERIOD, "RentalAgreement: period has not passed since last payment");
+        status = RentStatus.COMPLETED;
     }
 
     function reduceTerm(uint _periods) public active onlyParties {
@@ -145,12 +133,7 @@ contract RentalAgreement is PaymentSplitter {
         renewalRequested = 0;
     }
 
-    function writeReview(string memory _review) public onlyTenant {
-        require(status == RentStatus.TERMINATED, "RentalAgreement: rent agreement is not completed");
-        review = _review;
-    }
-
-    function paymentExpirationDate() public view returns (uint) {
+    function paymentDueDate() public view returns (uint) {
         return terms.startDate + paymentCounter * PERIOD;
     }
 
@@ -162,9 +145,22 @@ contract RentalAgreement is PaymentSplitter {
         return terms;
     }
 
-    function collect() public override {
+    function returnDeposit(uint _penalty) public onlyLandlord completed returns (uint) {
+        require(_penalty < terms.securityDeposit, "RentalAgreement: penalty must be less than the security deposit");
+        uint remaining = terms.securityDeposit - _penalty;
+        if (remaining > 0) {
+            IERC20(walletContractAddress()).transfer(tenant, remaining);
+        }
+        status = RentStatus.TERMINATED;
+        if (_penalty > 0) {
+            super.collect();
+        }
+        return remaining;
+    }
+
+    function collect() public override returns (uint) {
         require(status != RentStatus.ACTIVE, "RentalAgreement: rent agreement is still active");
-        super.collect();
+        return super.collect();
     }
 
     function pay(uint _amount) public pure override {
--- besu/src/contracts/SaleAgreement.sol
+++ besu/src/contracts/SaleAgreement.sol
@@ -4,11 +4,17 @@ pragma solidity ^0.8.0;
 import "./utils/Context.sol";
 import "./Ownership.sol";
 import "./interface/IERC20.sol";
+import "./interface/permissioning/IAccountRegistry.sol";
+import "./utils/Strings.sol";
 
 
 contract SaleAgreement is Context {
 
-    enum Status { PENDING, AGREED, COMPLETED, WIDTHDRAWN, BREACHED }
+    event PreSaleAgreement(address indexed buyer, address indexed seller, address indexed realty);
+    event DeedTransfer(address indexed buyer, address indexed seller, address indexed realty);
+    event SaleWithdrawal(address indexed buyer, address indexed seller, address indexed realty);
+
+    enum Status { PENDING, AGREED, COMPLETED, WITHDRAWN }
 
     struct SaleDetails {
         address buyer;
@@ -27,8 +33,9 @@ contract SaleAgreement is Context {
     Status public status;
     uint public createdAt;
 
-    bool private buyerSignature;
-    bool private sellerSignature;
+    Status private buyerStatus;
+    Status private sellerStatus;
+    uint penaltyRequest;
     
     modifier onlyParties() {
         require(msg.sender == details.buyer || msg.sender == details.seller, "SaleAgreement: only buyer or seller can call this function");
@@ -41,7 +48,7 @@ contract SaleAgreement is Context {
     }
 
     modifier privileged() { // TODO 
-        require(msg.sender == details.realtor, "SaleAgreement: only privileged entities can call this function");
+        require(msg.sender == details.buyer || msg.sender == details.seller || isPrivileged(msg.sender), "SaleAgreement: only privileged entities can call this function");
         _;
     }
 
@@ -52,28 +59,36 @@ contract SaleAgreement is Context {
         require(Ownership(_details.realty).shareOf(_details.seller) >= _details.share, "SaleAgreement: seller does not have enough shares");
         details = _details;
         status = Status.PENDING;
+        buyerStatus = Status.PENDING;
+        sellerStatus = Status.PENDING;
         createdAt = block.timestamp;
     }
 
-    function consent() public { // TODO mudar nome pre agreement, terms, consent, hold in escrow?
+    function consent() public onlyParties { // TODO mudar nome pre agreement, terms, consent, hold in escrow?
         require(status == Status.PENDING, "SaleAgreement: sale already agreed on");
-        Ownership(details.realty).transferShares(details.seller, address(this), details.share);
-        IERC20(walletContractAddress()).transferFrom(details.buyer, address(this), details.earnest);
-        status = Status.AGREED;
+        if (msg.sender == details.buyer) {
+            buyerStatus = Status.AGREED;
+        } else {
+            sellerStatus = Status.AGREED;
+        }
+        if (buyerStatus == Status.AGREED && sellerStatus == Status.AGREED) {
+            Ownership(details.realty).transferShares(details.seller, address(this), details.share);
+            IERC20(walletContractAddress()).transferFrom(details.buyer, address(this), details.earnest);
+            status = Status.AGREED;
+            emit PreSaleAgreement(details.buyer, details.seller, details.realty);
+        }
     }
 
     function commit() public onlyParties {
         require(status == Status.AGREED, "SaleAgreement: sale already processed or not yet agreed on");
 
         if (msg.sender == details.buyer) {
-            require(!buyerSignature, "SaleAgreement: this party has already signed");
-            buyerSignature = true;
+            buyerStatus = Status.COMPLETED;
         } else {
-            require(!sellerSignature, "SaleAgreement: this party has already signed");
-            sellerSignature = true;
+            sellerStatus = Status.COMPLETED;
         }
 
-        if (buyerSignature && sellerSignature) {
+        if (buyerStatus == Status.COMPLETED && sellerStatus == Status.COMPLETED) {
             transfer();
             status = Status.COMPLETED;
         }
@@ -85,25 +100,32 @@ contract SaleAgreement is Context {
         IERC20(walletContractAddress()).transfer(details.realtor, comm);
         IERC20(walletContractAddress()).transfer(details.seller, details.price - comm);
         Ownership(details.realty).transferShares(address(this), details.buyer, details.share);
+        emit DeedTransfer(details.buyer, details.seller, details.realty);
     }
 
-    function widthdraw() public privileged {
-        require(status == Status.AGREED, "SaleAgreement: sale already processed or not yet agreed on");
-        require(block.timestamp - createdAt < details.contengencyPeriod, "SaleAgreement: contengency period already over");
-        IERC20(walletContractAddress()).transfer(details.buyer, details.earnest);
-        Ownership(details.realty).transferShares(address(this), details.seller, details.share);
-        status = Status.WIDTHDRAWN;
-    }
-
-    function breach(uint _penalty) public privileged {
+    function withdraw(uint _penalty) public privileged {
         require(status == Status.AGREED, "SaleAgreement: sale already processed or not yet agreed on");
         require(_penalty <= details.earnest, "SaleAgreement: penalty can not be more than earnest");
-        IERC20(walletContractAddress()).transfer(details.seller, _penalty);
-        if (details.earnest - _penalty > 0) {
-            IERC20(walletContractAddress()).transfer(details.buyer, details.earnest - _penalty);
+        //require(block.timestamp - createdAt < details.contengencyPeriod, "SaleAgreement: contengency period already over");
+
+        if (msg.sender == details.buyer) {
+            buyerStatus = Status.WITHDRAWN;
+        } else if (msg.sender == details.seller) {
+            sellerStatus = Status.WITHDRAWN;
+            penaltyRequest = _penalty;
+        } else {
+            penaltyRequest = _penalty;
         }
-        Ownership(details.realty).transferShares(address(this), details.seller, details.share);
-        status = Status.BREACHED;
+
+        if ((buyerStatus == Status.WITHDRAWN && sellerStatus == Status.WITHDRAWN) || isPrivileged(msg.sender)) {
+            IERC20(walletContractAddress()).transfer(details.seller, penaltyRequest);
+            if (details.earnest - penaltyRequest > 0) {
+                IERC20(walletContractAddress()).transfer(details.buyer, details.earnest - penaltyRequest);
+            }
+            Ownership(details.realty).transferShares(address(this), details.seller, details.share);
+            status = Status.WITHDRAWN;
+            emit SaleWithdrawal(details.buyer, details.seller, details.realty);
+        }
     }
 
     // executa funcoes no contrato Ownership do realty
@@ -111,6 +133,10 @@ contract SaleAgreement is Context {
         require(status == Status.AGREED, "SaleAgreement: realty not held in escrow");
         (bool success, ) = details.realty.call{value: _value}(_data);
         require(success, "SaleAgreement: transaction failed");
+    }
+
+    function isPrivileged(address _address) private view returns (bool) {
+        return Strings.equals(IAccountRegistry(accountRegistryAddress()).orgOf(_address), "admin_org");
     }
   
 }
